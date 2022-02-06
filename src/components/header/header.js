95230a21cd4059d03c988d02c83ff4e34b3cb8df
Md. Joynal Abedin <joynal.9933@gmail.com>
2022-02-07 02:45:23 +0600

    header initial work

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+<header>
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <div className="site-info">
+                    <h1>Joynal Abedin</h1>
+                </div>
+            </div>
+            <div className="col">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="col">
+                <div className="work">Works</div>
+            </div>
+        </div>
+    </div>
+</header>
