140b54aab41fcdb6fcfd153d6e3a55bde36e9e4f
Md. Joynal Abedin <joynal.9933@gmail.com>
2022-02-07 02:32:52 +0600

    I am confused

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import ASD from './cc/asd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <ASD></ASD>
       </header>
     </div>
