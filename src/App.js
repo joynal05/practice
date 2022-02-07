bd482e5eb6477d4edcf87822a0b827507250950f
Md. Joynal Abedin <joynal.9933@gmail.com>
2022-02-07 21:38:28 +0600

    123

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header/header";
 import Body from "./components/body/body";
 import Footer from "./components/footer/footer";
+import './common.css'
 
 function App() {
   return (
