3c3f4e960ff1351140d35ce5626f19d277b99430
joynal9933 <joynal.9933@gmail.com>
2022-02-15 02:25:26 +0600

    not complited

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './common.css'
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [timeOn, setTimeOn] = useState(false);
+
+  useEffect(() => {
+
+    var timeCount = null;
+
+    if(timeOn){
+      timeCount = setInterval(() => {
+        setTime = setTime + 10;
+      }, 10);
+    }else{
+      clearInterval (timeCount);
+    }
+
+    
+  }, [timeOn]);
+
+
   return (
     <div className="App">
-
-
-
       
-
-
 
     </div>
   );
