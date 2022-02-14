5d9331e5a7c1da4c1f3787c3676a07c973e2e0e1
joynal9933 <joynal.9933@gmail.com>
2022-02-15 01:14:43 +0600

    practice SW

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Header from "./components/header/Header";
-import Body from "./components/body/body";
-import Footer from "./components/footer/footer";
 import './common.css'
 
 function App() {
   return (
     <div className="App">
 
-      <Body></Body>
+
+
+      
+
 
 
     </div>
